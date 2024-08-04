2492380c534c4b982cb9da943512156fb563d671
Bryce Thuilot <bryce@thuilot.io>
2024-08-04 17:28:30 -0400

    chore: add initial commit retrieval scaffolding
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/bthuilot/git-scanner/pkg/retrieval"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +13,19 @@ var rootCmd = &cobra.Command{
 	Use:   "git-scanner",
 	Short: "git-scanner searches *all* commits in a git repository for credentials",
 	Long:  `git-scanner searches *all* commits in a git repository for credentials, including orphaned commits.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		commits, err := retrieval.RetrieveAllCommits()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		logrus.Info("Retrieved all commits")
+		for _, commit := range commits {
+			logrus.Infof("Commit: %s", commit.Hash)
+		}
+
+		// TODO: scan each commit
+	},
 }
 
 func Execute() {
---
+++ pkg/retrieval/retrieve.go
@@ -0,0 +1,7 @@
+package retrieval
+
+import "github.com/go-git/go-git/v5/plumbing/object"
+
+func RetrieveAllCommits() ([]object.Commit, error) {
+	return nil, nil
+}
